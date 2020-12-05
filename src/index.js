0a398b93a1e82369b4e8614c795853b6be87b4e6
Rafael <rafaelfsen@hotmail.com>
2020-12-04 22:16:05 -0300

    botao de retorno e ajuste de erros

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 ReactDOM.render(
-  <App title= "Hello Gama Academy"/>,
+  <App />,
   document.getElementById('root')
 );
